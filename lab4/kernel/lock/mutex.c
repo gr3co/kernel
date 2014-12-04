3448c2e7d4b19fc45cf9d3dced4b241ecd109567
Stephen Greco <sgreco94@gmail.com>
2014-12-04 15:59:46 -0500

    Started implementing mutex

--- lab4/kernel/lock/mutex.c
+++ lab4/kernel/lock/mutex.c
@@ -21,22 +21,75 @@
 #include <exports.h> // temp
 #endif
 
+#define NULL 0
+
 mutex_t gtMutex[OS_NUM_MUTEX];
+static int next_mutex_available;
 
 void mutex_init()
 {
-	
+	int i;
+	for (i = 0; i < OS_NUM_MUTEX; i++) {
+		gtMutex[i].bAvailable = FALSE;
+		gtMutex[i].pHolding_Tcb = NULL;
+		gtMutex[i].bLock = 0;
+		gtMutex[i].pSleep_queue = NULL;
+	}
+	next_mutex_available = 0;
 }
 
 int mutex_create(void)
 {
-	
-	return 1; // fix this to return the correct value
+	if (next_mutex_available >= OS_NUM_MUTEX) {
+		printf("No mutexes remaining\n");
+		return;
+	}
+	gtMutex[next_mutex_available++].bAvailable = TRUE;
 }
 
-int mutex_lock(int mutex  __attribute__((unused)))
+int mutex_lock(int mutex)
 {
-	return 1; // fix this to return the correct value
+	if (mutex >= OS_NUM_MUTEX) {
+		printf("Invalid mutex number\n");
+		return -EINVAL;
+	}
+	if (gtMutex[mutex].bAvailable == FALSE) {
+		printf("Mutex unavailable\n");
+		return -EINVAL;
+	}
+	if (gtMutex[mutex].bLock == 1 
+		&& gtMutex[mutex].pHolding_Tcb == get_cur_tcb()) {
+		printf("Mutex already locked by this task\n");
+		return -EDEADLOCK;
+	}
+	// Mutex currently available
+	if (gtMutex[mutex].bLock == 0) {
+		gtMutex[mutex].bLock = 1;
+		gtMutex[mutex].pHolding_Tcb = get_cur_tcb();
+
+		// Do something to the tcb's priority too
+		
+		return 0;
+	// Add the current task to the mutex sleep queue
+	} else {
+		uint8_t current_priority = get_cur_prio();
+		tcb_t *current_task = get_cur_tcb();
+		tcb_t *sleep_task = gtMutex[mutex].pSleep_queue;
+		if (sleep_task == NULL || sleep_task->cur_prio > current_priority) {
+			gtMutex[mutex].pSleep_queue = current_task;
+			current_task->pSleep_queue = sleep_task;
+		} else {
+			// look for where the current task would fit into the queue
+			while (sleep_task->sleep_queue != NULL 
+				&& sleep_task->sleep_queue->cur_prio < current_priority) {
+				sleep_task = sleep_task->sleep_queue;
+			}
+			// and insert the task
+			tcb_t *next = sleep_task->sleep_queue;
+			sleep_task->sleep_queue = current_task;
+			current_task->sleep_queue = next;
+		}
+	}
 }
 
 int mutex_unlock(int mutex  __attribute__((unused)))
